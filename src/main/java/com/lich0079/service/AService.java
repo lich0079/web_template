a36f160b45f3e35b0037c481db180b642a9d6b12
310191911 <frank.zhang_2@philips.com>
2015-07-03 13:08:03 +0800

    add comment

--- src/main/java/com/lich0079/service/AService.java
+++ src/main/java/com/lich0079/service/AService.java
@@ -6,6 +6,11 @@ import java.util.List;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+/**
+ * the cache approach relay on spring proxy the class
+ * so it must be a spring inited object so spring can proxy it
+ * also the method must not be static
+ */
 @Service("aService")
 public class AService {
 
